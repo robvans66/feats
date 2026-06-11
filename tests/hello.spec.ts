a39c4154d62f75d4adb46f31d467224be889e390
robvans66 <robvans@gmail.com>
2026-06-11 14:36:21 +0200

    removed tests

--- tests/hello.spec.ts
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-describe('Hello World Tests', () => {
-    it('should return true for a simple assertion', () => {
-        expect(1 + 1).toBe(2);
-    });
-});
